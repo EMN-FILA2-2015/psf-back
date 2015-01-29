c3a91a49fdfd171a27cf70dd26f04041ea40839e
Rémy Ferré <remy-ferre@laposte.net>
2015-01-29 18:20:31 +0100

    **feat** Add more fields to Registry
    There are still the kind of Registry to add.

--- src/main/java/org/psf/domain/Registry.java
+++ src/main/java/org/psf/domain/Registry.java
@@ -3,6 +3,14 @@ package org.psf.domain;
 public class Registry {
 
     private final long id;
+    
+    private String name;
+        
+    private String host;
+    
+    private int port;
+    
+    private String protocol;
 
     public Registry(long id) {
         this.id = id;
@@ -11,4 +19,37 @@ public class Registry {
     public long getId() {
         return id;
     }
+    
+    public String getName() {
+    	return name;
+    }
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    
+    public String getHost() {
+    	return host;
+    }
+    
+    public void setHost(String host) {
+    	this.host = host;
+    }
+    
+    public int getPort() {
+    	return port;
+    }
+    
+    public void setPort(int port) {
+    	this.port = port;
+    }
+    
+    public String getProtocol() {
+    	return protocol;
+    }
+    
+    public void setProtocol(String protocol) {
+    	this.protocol = protocol;
+    }
+    
 }
